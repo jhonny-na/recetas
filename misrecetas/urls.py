079b09392c4743b9d93a045ca661e79d8a324883
jhonny-na <jhonnynavarrok@gmail.com>
2026-09-21 20:25:10 -0300

    Update urls.py

--- misrecetas/urls.py
+++ misrecetas/urls.py
@@ -18,9 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+
+from recetasApp.sitemaps import RecetaSitemap, PaginasEstaticasSitemap
+
+sitemaps = {
+    'recetas': RecetaSitemap,
+    'paginas': PaginasEstaticasSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('', include('recetasApp.urls')),
 ]
 
